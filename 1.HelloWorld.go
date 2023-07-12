2d1eb87d0c9e7aa920beff3482c428ca8db5f7c7
赵翔 <1084691005@qq.com>
2023-07-12 14:01:14 +0800

    first commit

---
+++ 1.HelloWorld.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func _json(c *gin.Context) {
+
+	type UserInfo struct {
+		Name string `json:"name"`
+		Age  int    `json:"age"`
+		// 不参与序列化 整个password都不会显示
+		Password string `json:"-"`
+	}
+
+	user := UserInfo{
+		"赵翔",
+		24,
+		"123456",
+	}
+
+	c.JSON(200, user)
+}
+
+func main() {
+	route := gin.Default()
+	route.GET("/index", func(context *gin.Context) {
+		context.String(http.StatusOK, "Hello World!")
+	})
+
+	route.GET("/json", _json)
+
+	route.Run(":7777")
+	// 本质就是对 http.ListenAndServe(":7777",route) 的进一步封装
+}
